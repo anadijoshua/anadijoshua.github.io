f803c751adfac07c76347b41e45c53927f7124c7
Anadi Joshua <joshuaanadi@gmail.com>
2023-04-13 22:34:23 +0100

    Uploading website

---
+++ script.js
@@ -0,0 +1,83 @@
+console.log('1');
+setTimeout(() => {
+    console.log('2');
+}, 4000) 
+console.log('3');
+console.log('4');
+
+
+
+// const flattened = [[0, 1], [2, 3], [4, 5]].reduce(
+//     (a, b) => a.concat(b), []
+// )
+
+// console.log(flattened)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// reference type
+// const object1 = { vaulue: 20};
+// const object2 = object1;
+// const object3 = {value: 20};
+
+
+// // intstanciation
+// class Player {
+//     constructor (name,type) {
+//         this.name = name;
+//         this.type = type;
+//     }
+//     instructions() {
+//         debugger;
+//         console.log(`Hello my name is ${this.name}, and ${this.type} is what i like`);
+//     }}
+
+
+// class Diciple extends Player {
+//     constructor(name, type) {
+//         super(name,type)
+//     }
+    
+//     greet(){
+//         console.log(`Hello there ${this.name} , this is ${this.type}`);
+//     }
+// }
+// const disciple1 = new Diciple("Joshua", "Praying to the Father");
+// const disciple2 = new Diciple("Joshua", "How i fight my battle");
+
+// console.log(disciple1.instructions());
+// console.log(disciple2.greet()); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ test.js
@@ -0,0 +1,85 @@
+// // // STRINGS
+// // numbers
+// // strings
+// // objects
+// // null
+// // undefined
+// // Arrays
+// // Date
+
+// body{
+//   margin: 0;
+//   background: #0F2027;  /* fallback for old browsers */
+// background: -webkit-linear-gradient(to right, #2C5364, #203A43, #0F2027);  /* Chrome 10-25, Safari 5.1-6 */
+// background: linear-gradient(to right, #2C5364, #203A43, #0F2027); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
+// }
+
+
+// /* For the Home tab Menu */
+// ul{
+//   width: 50em;
+//   display: flex;
+//   list-style: none;
+//   justify-content: end;
+//   float: right;
+//   border-bottom: 3px solid  #bdc3c7; 
+//   box-shadow: -8px 8px 10px -4px rgba(81,128,151,0.75);
+// -webkit-box-shadow: -8px 8px 10px -4px rgba(81,128,151,0.75);
+// -moz-box-shadow: -8px 8px 10px -4px rgba(81,128,151,0.75);
+// }
+
+// ul li{
+//   color: white;
+//   font-weight: bold;
+//   font-size: 20px;
+//   margin: 10px;
+//   font-family: 'Tangerine' Arial;
+
+// }
+
+// ul :last-child{
+//   background-color: red;
+//   padding: 5px;
+
+// }
+// /* full stop */
+
+// /* ___________________________________ */
+
+
+
+// /* For the picture */
+
+// .container{
+//   clear: both;
+//   display: flex;
+//   flex-wrap: wrap;
+//   justify-content: center;
+// }
+
+// .container #image{
+//   width: 300px;
+//   margin: 10px;
+// } 
+
+
+
+
+// /* footer */
+// .show{
+//   margin: 0 auto;
+// }
+// footer{
+//   clear: both;
+//   justify-self: center;
+//   width: 75rem;
+//   background: #0f0c29;  /* fallback for old browsers */
+//   background: -webkit-linear-gradient(to right, #24243e, #302b63, #0f0c29);  /* Chrome 10-25, Safari 5.1-6 */
+//   background: linear-gradient(to right, #24243e, #302b63, #0f0c29); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+//   padding: 30px;
+// }
+
+// footer button{
+//   color: white;
+// }
